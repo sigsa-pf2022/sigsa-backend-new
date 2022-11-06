c4d2fbf1776a6c9503ebf62963015517c8a06d49
Matias Bruno <matiasbruno97@gmail.com>
2022-11-06 19:38:46 -0300

    Fix: config mail module

---
+++ src/config/mail.config.ts
@@ -0,0 +1,42 @@
+import { MailerOptions } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MailerAsyncOptions } from '@nestjs-modules/mailer/dist/interfaces/mailer-async-options.interface';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+
+export default class MailConfig {
+  static getMailConfig(configService: ConfigService): MailerOptions {
+    const url = __dirname.split('/');
+    url.pop();
+    url.push('mail');
+    const dir = url.join('/');
+    return {
+      transport: {
+        host: configService.get('MAILER_HOST'),
+        port: 465,
+        secure: true,
+        auth: {
+          user: configService.get('MAILER_USER'),
+          pass: configService.get('MAILER_PASSWORD'),
+        },
+      },
+      defaults: {
+        from: '"No Reply" <noreply@example.com>',
+      },
+      template: {
+        dir: join(dir, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    };
+  }
+}
+
+export const mailConfigAsync: MailerAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService): Promise<MailerOptions> =>
+    MailConfig.getMailConfig(configService),
+  inject: [ConfigService],
+};
--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,32 +1,11 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
-import { join } from 'path';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { MailService } from './mail.service';
+import { mailConfigAsync } from 'src/config/mail.config';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.MAILER_HOST,
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.MAILER_USER,
-          pass: process.env.MAILER_PASSWORD,
-        },
-      },
-      defaults: {
-        from: '"No Reply" <noreply@example.com>',
-      },
-      template: {
-        dir: join(__dirname, 'templates'),
-        adapter: new HandlebarsAdapter(), 
-        options: {
-          strict: true,
-        },
-      },
-    }),
+    MailerModule.forRootAsync(mailConfigAsync),
   ],
   providers: [MailService],
   exports: [MailService],
