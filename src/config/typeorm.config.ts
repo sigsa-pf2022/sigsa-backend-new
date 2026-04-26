b4b9496e222279c1c29353c2463fe282cc81c1c6
Pedro Martinez <77643415+pedro-mar99@users.noreply.github.com>
2026-04-26 19:30:16 -0300

    feat: add crypto polyfill and update TypeOrm configuration for improved database handling (#13)

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -10,23 +10,27 @@ import { SeederOptions } from 'typeorm-extension';
 
 export default class TypeOrmConfig {
   static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions & SeederOptions {
-    const url = __dirname.split('\\');
-    url.pop()
-    const dir = url.join('\\');
     return {
       type: 'postgres',
       host: configService.get('DB_HOST'),
-      port: Number(configService.get('DB_PORT)')),
+      port: Number(configService.get('DB_PORT')),
       username: configService.get('DB_USERNAME'),
       password: configService.get('DB_PASSWORD'),
-      database: 'sigsa_db',
-      entities: [dir + '/**/*.entity{.ts,.js}'],
+      database: configService.get('DB_NAME') || 'sigsa_db',
+      autoLoadEntities: true,
       seeds: [CountrySeeder, StatesSeeder, SpecializationsSeeder],
       synchronize: true,
       logging: false,
       // dropSchema: true,
     };
+    // config para windows
+    // const url = __dirname.split('\\');
+    // url.pop()
+    // const dir = url.join('\\');
+    // database: 'sigsa_db',
+    // entities: [dir + '/**/*.entity{.ts,.js}'],
   }
+  
 }
 
 export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
